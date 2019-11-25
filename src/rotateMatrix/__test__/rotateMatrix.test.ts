aa4041065b4c09acd1deea44426de1829e8177b2
nduy79 <ngocduy0997@gmail.com>
2019-11-26 01:08:22 +0700

    modify unit-test

--- src/rotateMatrix/__test__/rotateMatrix.test.ts
+++ src/rotateMatrix/__test__/rotateMatrix.test.ts
@@ -1,6 +1,30 @@
-import {rotateMatrix }from "../rotateMatrix";
-
+import { rotateMatrix } from "../rotateMatrix";
 describe("test suite", () => {
+  it("rotate not square matrix should return error", () => {
+    const grid = [
+      [1, 2, 3],
+      [4, 5, 6]
+    ];
+    try {
+      rotateMatrix(grid, 1);
+    } catch (error) {
+      expect(error).toThrowError(new Error("Not square gird"));
+    }
+  });
+
+  it("rotate with nagative number of rotation should return error", () => {
+    const grid = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ];
+    try {
+      rotateMatrix(grid, -1);
+    } catch (error) {
+      expect(error).toThrowError(new Error("Not accept negative number of rotations"));
+    }
+
+  });
   it("rotate 1 time", () => {
     const grid = [
       [1, 2, 3],
@@ -13,35 +37,34 @@ describe("test suite", () => {
       [8, 5, 2],
       [9, 6, 3]
     ];
-    expect(result).toBe(expectResult);
+    expect(result).toStrictEqual(expectResult);
   });
-
-  it("rotate 2 time", () => {
+  it("rotate 2 times", () => {
     const grid = [
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9]
     ];
-    const result = rotateMatrix(grid, 1);
+    const result = rotateMatrix(grid, 2);
     const expectResult = [
       [9, 8, 7],
       [6, 5, 4],
       [3, 2, 1]
     ];
-    expect(result).toBe(expectResult);
+    expect(result).toStrictEqual(expectResult);
   });
-  it("rotate 3 time", () => {
+  it("rotate 3 times", () => {
     const grid = [
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9]
     ];
-    const result = rotateMatrix(grid, 1);
+    const result = rotateMatrix(grid, 3);
     const expectResult = [
       [3, 6, 9],
       [2, 5, 8],
       [1, 4, 7]
     ];
-    expect(result).toBe(expectResult);
+    expect(result).toStrictEqual(expectResult);
   });
 });
